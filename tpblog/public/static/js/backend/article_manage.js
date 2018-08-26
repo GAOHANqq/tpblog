1b21f767c9001a1e511614ead93cef5d3d5ac7bf
GAOHANqq <1271448267@qq.com>
2018-08-26 21:16:09 +0800

    markdown编辑器

--- tpblog/public/static/js/backend/article_manage.js
+++ tpblog/public/static/js/backend/article_manage.js
@@ -1,9 +1,17 @@
 $(function(){
-
 	$('.btn-delete').on('click' ,function() {
 		if (!confirm('确定要删除吗')) {
 			return false;
 		}
 	})
+	// 检查标题
+	// 检查内容
+
+	var editor = editormd("editormd", {
+		width   : "100%",
+		height  : 400,
+		syncScrolling : "single",
+		path    : "/static/libs/editormd/lib/"
+	});
 
 });
